40a138a08420fc13f4444df3321f4a4f8dc7b939
Tony Wasserka <neobrain@users.noreply.github.com>
2025-08-19 09:41:01 +0200

    Add code to generate shared font CHN/KOR/TWN titles

--- shared_font/run.sh
+++ shared_font/run.sh
@@ -14,8 +14,16 @@ echo "Convert the bcfnt file into a romfs..."
 mkdir romfs
 ./../common/lz_compress.py code.bcfnt romfs/cbf_std.bcfnt.lz
 
-echo "Create shared font title..."
+echo "Create shared font main title..."
 makerom -rsf ../info.rsf -o 00000000.app
+
+echo "Create shared font CHN/KOR/TWN titles..."
+mv romfs/*.bcfnt.lz romfs/cbf_zh-Hans-CN.bcfnt.lz
+makerom -rsf ../info.rsf -o 00000000_chn.app
+mv romfs/*.bcfnt.lz romfs/cbf_ko-Hang-KR.bcfnt.lz
+makerom -rsf ../info.rsf -o 00000000_kor.app
+mv romfs/*.bcfnt.lz romfs/cbf_zh-Hant-TW.bcfnt.lz
+makerom -rsf ../info.rsf -o 00000000_twn.app
 
 echo "Cleanups..."
 rm -rf romfs
